03c487c46be403dbec2fbfed01440e1a7f622644
printminion <m.kupriyanov@gmail.com>
2025-01-18 15:48:43 +0100

    feature(display-version): add version output

---
+++ src/include/version.h
@@ -0,0 +1,17 @@
+/**
+ * @file version.h
+ * Copyright 2025 @printminion
+ */
+
+#ifndef _APP_VERSION_H_
+#define _APP_VERSION_H_
+
+#define APP_SHA_SHORT                      // Filled by Github sed
+
+const uint32_t APP_VERSION = 0x01000001;   // 1.0.0.1
+
+#endif  // _APP_VERSION_H_
+
+#ifndef CODE_IMAGE_STR
+  #define CODE_IMAGE_STR "dev"
+#endif
